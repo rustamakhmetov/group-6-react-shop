6575df53f9c597da24eda987bc992be8e77c9175
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2019-05-10 15:05:11 +0500

    [task3] add CartContainer

---
+++ src/containers/CartContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CartContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    }
+  }
+}
+
+export default CartContainer;
